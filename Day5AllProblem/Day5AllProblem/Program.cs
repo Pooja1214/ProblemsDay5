c1053dfd0504f20e10f9b53da6d00d0d7498db45
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-03-15 17:01:27 +0530

    HeadTailPercentage : Head Tail Percentage

--- Day5AllProblem/Day5AllProblem/Program.cs
+++ Day5AllProblem/Day5AllProblem/Program.cs
@@ -1,18 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-//Program for compute Quotient and Remainder
+//program for head tail percentage calculation
 
-int divident, divisor;
-//int quotient, remainder; Declare variables here also
+Console.WriteLine("Let's flip a coin N times");
+{
+    Random Rand = new Random();
 
-Console.WriteLine("Enter Divident");
-divident = Convert.ToInt32(Console.ReadLine());
+    int heads = 0;
+    int tails = 0;
+    int result = 0;
+    Console.WriteLine("enter how many times coin flip");
+    int N = Convert.ToInt16(Console.ReadLine());
 
-Console.WriteLine("Enter Divisor");
-divisor = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < N; i++)
+    {
+        result = Rand.Next(0, 2);
+        if (result == 1)
+        {
+            heads++;
+        }
+        else
+        {
+            tails++;
+        }
+    }
 
-int quotient = divident / divisor;
-int remainder = divident % divisor;
+    Console.WriteLine("heads was flipped {0} times", +heads);
+    Console.WriteLine("tails was flipped {0} times", +tails);
+    return result;
+}
 
-Console.WriteLine("quotient = " + quotient);
-Console.WriteLine("remainder = " + remainder);
