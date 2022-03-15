a77d27bba869c3917c8b6c4878ec1897c5bf833e
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-03-15 16:33:01 +0530

    GreaterNumber : Greater Between 3 Numbers

--- Day5AllProblem/Day5AllProblem/Program.cs
+++ Day5AllProblem/Day5AllProblem/Program.cs
@@ -1,23 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-// Swapping of two numbers
+//Largest number among 3 numbers
 
-
-Console.WriteLine("enter First Number:");
+Console.WriteLine("Enter value for num1: ");
 int num1 = Convert.ToInt16(Console.ReadLine());
 
-Console.WriteLine("enter Second Number:");
+Console.WriteLine("Enter value for num2: ");
 int num2 = Convert.ToInt16(Console.ReadLine());
 
-int temp = Convert.ToInt16(Console.ReadLine());
-
-temp = num1;
-num1 = num2;
-num2 = temp;
-
-Console.WriteLine("After Swapping number");
-Console.WriteLine("First Number:"+ num1 );
-Console.WriteLine("Second Number:" + num2);
+Console.WriteLine("Enter value for num3: ");
+int num3 = Convert.ToInt16(Console.ReadLine());
 
 
 
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("num1 is greater");
+}
+else if (num2 > num3 && num2 > num1)
+{
+    Console.WriteLine("num2 is greater");
+}
+else
+{
+    Console.WriteLine("num3 is greater");
+}
